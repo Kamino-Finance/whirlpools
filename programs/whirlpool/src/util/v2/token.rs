cc092a915c67d6b1b57973e01688f79a9551b80d
Silviu Troscot <silviu.troscot10@gmail.com>
2025-09-30 13:30:48 +0300

    .

--- programs/whirlpool/src/util/v2/token.rs
+++ programs/whirlpool/src/util/v2/token.rs
@@ -296,10 +296,10 @@ pub fn is_token_badge_initialized<'info>(
     //     &mut token_badge.data.borrow().as_ref()
     // )?;
 
-    // msg!("token_badge.whirlpools_config: {:?}", token_badge.whirlpools_config);
-    // msg!("token_badge.token_mint: {:?}", token_badge.token_mint);
-    // msg!("whirlpools_config_key: {:?}", whirlpools_config_key);
-    // msg!("token_mint_key: {:?}", token_mint_key);
+    msg!("token_badge.whirlpools_config: {:?}", token_badge.whirlpools_config);
+    msg!("token_badge.token_mint: {:?}", token_badge.token_mint);
+    msg!("whirlpools_config_key: {:?}", whirlpools_config_key);
+    msg!("token_mint_key: {:?}", token_mint_key);
     // Ok(
     //     token_badge.whirlpools_config == whirlpools_config_key &&
     //     token_badge.token_mint == token_mint_key
